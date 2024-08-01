39e3186dd953a830862a2c5d11915b5c1bfaed8f
Ronny Chevalier <ronny@chevalier.io>
2024-08-02 01:55:59 +0200

    test(json): add a test to check for the strings extracted

--- src/lang/json.rs
+++ src/lang/json.rs
@@ -13,3 +13,61 @@ impl Language {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::lang::LintableString;
+    use crate::SharedSource;
+
+    use super::Language;
+
+    #[test]
+    fn lintable_strings() {
+        let json = r#"
+{
+    "field": "content",
+    "another_field": 1234,
+    "dict": {
+        "boolean": false,
+        "array": ["data"]
+    }
+}
+"#;
+        let json = SharedSource::new("file.json", json.as_bytes().to_vec());
+        let mut parsed = Language::json().parse(&json).unwrap();
+        let strings = parsed.strings(json.as_ref()).collect::<Vec<_>>();
+        assert_eq!(
+            strings,
+            [
+                LintableString {
+                    offset: 8,
+                    value: "field".into()
+                },
+                LintableString {
+                    offset: 17,
+                    value: "content".into()
+                },
+                LintableString {
+                    offset: 32,
+                    value: "another_field".into()
+                },
+                LintableString {
+                    offset: 59,
+                    value: "dict".into()
+                },
+                LintableString {
+                    offset: 77,
+                    value: "boolean".into()
+                },
+                LintableString {
+                    offset: 103,
+                    value: "array".into()
+                },
+                LintableString {
+                    offset: 113,
+                    value: "data".into()
+                }
+            ]
+        );
+    }
+}
